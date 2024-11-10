e045d9afbc7785a146cc9550d5e948640956dcfd
fsikali <flemmingsikali499@gmail.com>
2024-11-10 23:20:55 +0300

    Inheritance/src/practice_1/Main.java -> Inheritance/src/Java/Main.java

--- Inheritance/src/Java/Main.java
+++ Inheritance/src/Java/Main.java
@@ -1,7 +1,8 @@
-package practice_1;
+package Java;
 
 class Vehicle {
     protected String brand = "Ford";
+    public String engine;
     public void speed() {
         System.out.println("80KM");
     }
@@ -13,17 +14,42 @@ class Car extends Vehicle {
     public void check() {
         System.out.println("Hi Flemming");
     }
+
+    public void engine_type() {
+        System.out.println("The engine type is " + engine);
+    }
 }
 
 public class Main {
     public static void main(String[] args) {
         Car mycar = new Car() ;
 
+        mycar.engine = "TX";
+        mycar.engine_type();
         mycar.speed();
         mycar.check();
 
         System.out.println(mycar.brand + " " +  mycar.modelName);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
--- src/Threads_b/Main.java
+++
@@ -1,8 +0,0 @@
-package Threads_b;
-
-// Creating a thread to implement a runnable interface
-public class Main implements Runnable {
-    public void run() {
-        System.out.println("This code is running in a thread");
-    }
-}
